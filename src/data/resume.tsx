d28ca1b2267169b5769d8d302b1b9097f6893a4e
Umar Farooq <ufservices465@gmail.com>
2025-02-26 16:17:17 +0530

    fix: update project image path in resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -188,7 +188,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "/.",
+      image: "/reelpro.png",
       video: "",
     },
     {
